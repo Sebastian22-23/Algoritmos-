43cd6339a1d2729191addc813b49803575cdcbf8
Sebastián <fersebas502@gmail.com>
2024-09-19 16:53:27 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,26 @@
 #include <windows.h>
 #include <iostream>
 #include"Mapa1.h"
+#include"Mapa3.h"
 #include"Personaje.h"
 #include"conio.h"
+using namespace System;
 void MAPA1() {
     Console::CursorVisible = false;
     juego1* objJUEGO;
+    juego3* hola;
+
+    hola = new juego3();
 
     objJUEGO = new juego1();
 
-    objJUEGO->movimiento();
-    objJUEGO->perdiste();
+    //objJUEGO->movimiento();
+    //objJUEGO->perdiste();
+    hola->movimiento3();
 }
 int main() {
     Console::SetWindowSize(119, 34);
-    mapa1();
+    //mapa1();
     MAPA1();
     _getch();
     return 0;
